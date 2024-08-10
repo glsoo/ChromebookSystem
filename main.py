1284830b54ecd0e6e3a53acfcf091c17a4903f0b
NuLLKiD (NLKD) <52257398+nullkid10@users.noreply.github.com>
2024-08-10 16:32:52 -0400

    edited main

---
+++ main.py
@@ -0,0 +1,7 @@
+import time, tkinter, requests
+from tkinter import messagebox
+
+window = tkinter.Tk(className="Chromebook Management System")
+window.geometry("300x200")
+window.title("Chromebook Management System")
+window.mainloop()
