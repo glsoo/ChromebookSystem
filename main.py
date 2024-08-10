2abb7cd9d32b298653a074db4b8b3419271a783e
nullkid10 <52257398+nullkid10@users.noreply.github.com>
2024-08-10 19:44:08 -0400

    added comments, added icon, made cmsicon.png asset the window icon

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+#file setup
 import time, tkinter
 from tkinter import *
 
+#define variables
+icon = PhotoImage(file="/assets/cmsicon.png")
 win = Tk()
-win.geometry("300x200")
+window_width = round(win.winfo_screenwidth() * 0.60)
+window_height = round(win.winfo_screenheight() * 0.60)
+window_resolution = str(window_width)+"x"+str(window_height)
+win.geometry(window_resolution)
 win.title("Chromebook Management System")
+win.iconphoto(False, icon)
 
-
-
+#start the Tk mainloop
 win.mainloop()
 
