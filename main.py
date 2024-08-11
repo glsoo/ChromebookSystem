7679e12933f29b3d2bfb3d33f0dfba2521091925
nullkid10 <52257398+nullkid10@users.noreply.github.com>
2024-08-11 15:44:16 -0400

    added buttons and input frame

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ window_resolution = str(window_width)+"x"+str(window_height)
 win.geometry(window_resolution)
 win.title("Chromebook Management System")
 win.iconphoto(True, icon)
-label = Label(win, text="BMCCHS Chromebook Management System", font=("Arial", 16, "bold"), fg="black")
-
+title = Label(win, text="BMCCHS Chromebook Management System", font=("Arial", 16, "bold"), fg="black")
+buttonF = Frame(win)
+lendB = Button(buttonF, text="Lend", font=("Arial", 16), fg="white", bg="black")
+receiveB = Button(buttonF, text="Receive", font=("Arial", 16), fg="white", bg="black")
+identry = Entry(win, font=("Arial", 16), fg="black")
 # Pack the label into the window
-label.pack(pady=20)  # Add some padding to the top
-
+title.pack(pady=20)  # Add some padding to the top
+buttonF.pack()
+lendB.pack(side="left", padx=10)
+receiveB.pack(side="right", padx=10)
+identry.pack(pady=20)
 #start the Tk mainloop
 win.mainloop()
 
