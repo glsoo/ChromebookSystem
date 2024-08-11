0cf67c4c7daa6e8d5718f32173d8cf6b36766850
nullkid10 <52257398+nullkid10@users.noreply.github.com>
2024-08-10 20:46:15 -0400

    edited the logo and fixed a bug that prevented launch

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import time, tkinter
 from tkinter import *
 
 #define variables
-icon = PhotoImage(file="/assets/cmsicon.png")
 win = Tk()
+icon = PhotoImage(file="./assets/cmsiconr2.png")
 window_width = round(win.winfo_screenwidth() * 0.60)
 window_height = round(win.winfo_screenheight() * 0.60)
 window_resolution = str(window_width)+"x"+str(window_height)
 win.geometry(window_resolution)
 win.title("Chromebook Management System")
-win.iconphoto(False, icon)
+win.iconphoto(True, icon)
 
 #start the Tk mainloop
 win.mainloop()
